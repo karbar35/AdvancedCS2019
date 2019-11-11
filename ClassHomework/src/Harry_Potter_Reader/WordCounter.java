32f46830c045b1227482ea04fb3c6ab505403547
karbar35 <kartikdonepudi@gmail.com>
2019-11-11 02:56:33 -0500

    work on harry potter reading

---
+++ ClassHomework/src/Harry_Potter_Reader/WordCounter.java
@@ -0,0 +1,51 @@
+package Harry_Potter_Reader;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class WordCounter {
+	static HashMap<String, Integer> wordCount = new HashMap<String, Integer>();
+	
+	public static void main(String[] args) {
+		try {
+			
+			File script = new File("harry_potter");
+			Scanner s = new Scanner(script);
+			System.out.println("file is open");
+			
+			while(s.hasNext()) {
+				String newWord = s.next();
+				newWord = stripCharacters(newWord);
+				addToCount(newWord);
+			}
+			printWords();
+			s.close();
+			
+			
+		} catch (Exception e) {
+			System.out.println("error opening the file");
+		}
+		
+	}
+	
+	public static void addToCount(String word) {
+		if(wordCount.containsKey(word)) {
+			wordCount.replace(word, wordCount.get(word)+1);
+		} else {
+			wordCount.put(word, 1);
+		}
+	}
+	
+	public static String stripCharacters(String s) {
+		s = s.trim();
+		s = s.replaceAll("[^a-zA-Z ]", "").toLowerCase();
+		return s;
+	}
+
+	public static void printWords() {
+		for(String s : wordCount.keySet()) {
+			System.out.println(s + " : " + wordCount.get(s));
+		}
+	}
+}
