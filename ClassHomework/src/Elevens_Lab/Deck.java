0c979b8d8ff92b3dafd4f18b4f59c0937732766d
karbar35 <kartikdonepudi@gmail.com>
2019-11-13 10:11:03 -0500

    Finish deck class

---
+++ ClassHomework/src/Elevens_Lab/Deck.java
@@ -0,0 +1,37 @@
+package Elevens_Lab;
+
+import java.util.ArrayList;
+
+public class Deck {
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	private int size;
+	
+	public Deck(String[] ranks, String[] suits, int[] pointValues) {
+		for(String suit : suits) {
+			for(int i = 0; i < ranks.length; i++) {
+				cards.add(new Card(ranks[i], suit, pointValues[i]));
+			}
+		}
+		size = cards.size();
+		//this.shuffle();
+	}
+	
+	public boolean isEmpty() {
+		return cards.size() == 0;
+	}
+	
+	public int size() {
+		return size;
+	}
+	
+	public void shuffle() {
+		// shuffle code
+		size = cards.size();
+	}
+	
+	public Card deal() {
+		size--;
+		if(size < 0) return null;
+		return cards.get(size);
+	}
+}
